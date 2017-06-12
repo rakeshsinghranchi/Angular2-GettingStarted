fae7da5bbf5c522b510194df0727de4c31d4765e
Rakesh Ranjan Singh <raksingh@microsoft.com>
2017-06-12 00:23:02 -0700

    Services and Dependency Injection

--- APM/app/app.module.ts
+++ APM/app/app.module.ts
@@ -6,10 +6,12 @@ import { ProductListComponent } from "./products/product-list.component";
 import { FormsModule } from '@angular/forms';
 import { ProductFilterPipe } from "./products/product-filter.pipe";
 import { StarComponent } from "./shared/star.component";
+import { ProductService } from "./products/product.service";
 
 @NgModule({
   imports: [ BrowserModule,FormsModule ],
   declarations: [ AppComponent,ProductListComponent,ProductFilterPipe,StarComponent ],
-  bootstrap: [ AppComponent ]
+  bootstrap: [ AppComponent ],
+  providers:[ProductService]
 })
 export class AppModule { }
